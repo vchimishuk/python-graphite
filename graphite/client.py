3959532abae7fc7cf7fcb25600d8b7bf2cd0aa0e
Viacheslav Chimishuk <vchimishuk@yandex.ru>
2021-06-24 21:16:03 +0300

    Initial commit.

---
+++ graphite/client.py
@@ -0,0 +1,101 @@
+import io
+import time
+import datetime
+import socket
+import threading
+import logging
+from .metrics import Counter, Gauge, Series
+
+
+logger = logging.getLogger(__name__)
+
+
+class Graphite:
+    DEBT_MAX_SIZE = 100_000
+
+    def __init__(self, host, port, interval=60, timeout=10, prefix=''):
+        self.host = host
+        self.port = port
+        self.interval = interval
+        self.timeout = timeout
+        self.prefix = prefix
+        self.metrics = {}
+        self.metrics_lock = threading.Lock()
+        self.sender_shutdown = threading.Event()
+        self.sender = threading.Thread(target=self.send, daemon=True)
+        self.sender.start()
+
+    def __del__(self):
+        self.close()
+
+    def counter(self, name):
+        return self.metric(name, Counter)
+
+    def gauge(self, name, value):
+        return self.metric(name, lambda n: Gauge(n, value))
+
+    def series(self, name):
+        return self.metric(name, Series)
+
+    def close(self):
+        self.sender_shutdown.set()
+        self.sender.join()
+
+    def metric(self, name, clazz):
+        with self.metrics_lock:
+            if name not in self.metrics:
+                m = clazz(name)
+                self.metrics[name] = m
+                return m
+            else:
+                return self.metrics[name]
+
+    def send(self):
+        debt = []
+        last_time = time.time()
+
+        while True:
+            next_time = last_time + self.interval
+            self.sender_shutdown.wait(next_time - time.time())
+            last_time = next_time
+
+            with self.metrics_lock:
+                t = int(time.time())
+                sn = debt
+                for m in self.metrics.values():
+                    sn.extend(m.snapshot())
+
+            try:
+                hp = (self.host, self.port)
+                with socket.create_connection(hp, self.timeout) as s:
+                    s.sendall(self.serialize(sn).encode())
+                debt = []
+            except Exception as e:
+                logger.warn('Failed to send metrics: %s', e)
+                debt = sn[-self.DEBT_MAX_SIZE:]
+                raise e
+
+            if self.sender_shutdown.is_set():
+                break
+
+    def serialize(self, metrics):
+        t = str(int(time.time()))
+        s = io.StringIO()
+        for m in metrics:
+            if self.prefix:
+                s.write(self.prefix)
+                s.write('.')
+            s.write(m[0])
+            s.write(' ')
+            s.write(str(m[1]))
+            s.write(' ')
+            s.write(t)
+            s.write('\n')
+
+        return s.getvalue()
+
+    def delay(self):
+        now = datetime.datetime.now()
+        next_min = now.replace(second=0) + datetime.timedelta(minutes=1)
+
+        return (next_min - now).total_seconds()
---
+++ graphite/metrics.py
@@ -0,0 +1,97 @@
+import random
+import threading
+
+
+class Metric:
+    def __init__(self, name):
+        self.name = name
+
+    def snapshot(self):
+        raise NotImplemented()
+
+
+class Counter(Metric):
+    def __init__(self, name):
+        super().__init__(name)
+        self.value = 0
+        self.lock = threading.Lock()
+
+    def inc(self, delta=1):
+        with self.lock:
+            self.value += delta
+
+    def snapshot(self):
+        with self.lock:
+            v = self.value
+            self.value = 0
+
+            return ((self.name, v),)
+
+
+class Gauge(Metric):
+    def __init__(self, name, value):
+        super().__init__(name)
+        self.value = value
+
+    def snapshot(self):
+        return ((self.name, self.value()),)
+
+
+class Series(Metric):
+    MAX_SAMPLES = 1000
+    INF = float("inf")
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.lock = threading.Lock()
+        self.samples = [None] * self.MAX_SAMPLES
+        self.count = 0
+        self.sum = 0
+        self.min = 0
+        self.max = 0
+
+    def add(self, value):
+        with self.lock:
+            self.sum += value
+            if self.count == 0 or value < self.min:
+                self.min = value
+            if self.count == 0 or value > self.max:
+                self.max = value
+            # Add new sample or replace one of the existing N samples
+            # with probability MAX_SAMPLES/count otherwise.
+            if self.count < self.MAX_SAMPLES:
+                self.samples[self.count] = value
+            else:
+                if random.random() < (self.MAX_SAMPLES / float(count)):
+                    i = int(random.random() * self.MAX_SAMPLES)
+                    self.samples[i] = value
+            self.count += 1
+
+    def snapshot(self):
+        with self.lock:
+            s = []
+            s.append((self.name + '.sum', self.sum))
+            s.append((self.name + '.count', self.count))
+            s.append((self.name + '.min', self.min))
+            s.append((self.name + '.max', self.max))
+            if self.count:
+                i = min(self.count, self.MAX_SAMPLES)
+                self.samples.sort(key=lambda x: x if x is not None else self.INF)
+                s.append((self.name + '.pct50', self.samples[int(i / 2)]))
+                s.append((self.name + '.pct75', self.samples[int(i * 3 / 4)]))
+                s.append((self.name + '.pct95', self.samples[int(i * 95 / 100)]))
+                s.append((self.name + '.pct99', self.samples[int(i * 99 / 100)]))
+                s.append((self.name + '.avg', self.sum / self.count))
+            else:
+                s.append((self.name + '.pct50', 0.0));
+                s.append((self.name + '.pct75', 0.0));
+                s.append((self.name + '.pct95', 0.0));
+                s.append((self.name + '.pct99', 0.0));
+                s.append((self.name + '.avg', 0.0));
+
+            self.count = 0;
+            self.sum = 0;
+            self.min = 0;
+            self.max = 0;
+
+            return s
